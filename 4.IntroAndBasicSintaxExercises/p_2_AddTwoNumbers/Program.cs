f88f44afba0e67601fb55799b8c5dafacf16c740
Anna S. Kalinova <32344313+AnnaSKalinova@users.noreply.github.com>
2017-09-30 10:13:18 +0000

    Add files via upload

---
+++ 4.IntroAndBasicSintaxExercises/p_2_AddTwoNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p_2_AddTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstNum = int.Parse(Console.ReadLine());
+            var secondNum = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0} + {1} = {2}", firstNum, secondNum, firstNum + secondNum);
+        
+        }
+    }
+}
